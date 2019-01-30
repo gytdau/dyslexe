f346ecc336f9301355227ef4cfe8907ea82ac263
Gytis Daujotas <gytdau@gmail.com>
2019-01-30 21:49:07 +0000

    Added material design icons iconography

--- public/static/js/background.js
+++ public/static/js/background.js
@@ -1,3 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript(null, { file: "static/js/content.js" });
+    chrome.tabs.insertCSS(tab.id, {
+        file: "/static/css/content.css"
+    });
+    chrome.tabs.insertCSS(tab.id, {
+        file: "/static/css/iconography.css"
+    });
 });
